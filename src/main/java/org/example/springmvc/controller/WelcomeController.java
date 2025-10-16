9dae9a874d43c66332c6254ad28e456dbd0eb439
brunstart <brunstart@gmail.com>
2025-10-16 10:39:06 +0900

    로그 출력, RequestParam 추가

--- src/main/java/org/example/springmvc/controller/WelcomeController.java
+++ src/main/java/org/example/springmvc/controller/WelcomeController.java
@@ -1,12 +1,26 @@
 package org.example.springmvc.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@Slf4j
 public class WelcomeController {
+    // Logger log = LoggerFactory.getLogger(WelcomeController.class);
+
     @GetMapping("/")        // WelcomeController
-    public String index() {
+    public String index(/*HttpServletRequest req*/ @RequestParam("message") String msg) {
+
+        log.info("로그 출력 test SLF4J");
+        // System.out.println(req.getRequestURI());
+        // System.out.println(req.getParameter("message"));
+        System.out.println(msg);
+
         return "index";
     }
 }
